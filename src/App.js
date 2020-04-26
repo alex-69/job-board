4914b4d62c502a7095db668b383360f368e441c1
alex-69 <45286757+alex-69@users.noreply.github.com>
2020-04-26 10:10:33 +0200

    :feat: (router) set up rooter

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Redirect, Switch} from 'react-router-dom';
 
 import Board from './Board';
 
 
 const App = () => {
   return (
-    <Board/>
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          <Board/>
+        </Route>
+        <Redirect to="/"/>
+      </Switch>
+    </Router>
   );
 }
 
